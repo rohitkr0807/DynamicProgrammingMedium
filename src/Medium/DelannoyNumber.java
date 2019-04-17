7ade6bd01ffefa6e065141cc8e7847586d41efd5
Rohit Kumar <rohit.kumar@airpay.co.in>
2019-04-17 14:59:05 +0530

    DP2
    Signed-off-by: Rohit Kumar <rohit.kumar@airpay.co.in>

---
+++ src/Medium/DelannoyNumber.java
@@ -0,0 +1,63 @@
+package Medium;
+/*
+In mathematics, a Delannoy number D describes the number of paths from the southwest corner (0, 0) of a rectangular grid to the northeast corner (m, n), using only single steps north, northeast, or east.
+For Example, D(3, 3) equals 63.
+
+Delannoy Number can be calculated by:
+
+Delannoy number can be used to find:
+
+Counts the number of global alignments of two sequences of lengths m and n.
+Number of points in an m-dimensional integer lattice that are at most n steps from the origin.
+In cellular automata, the number of cells in an m-dimensional von Neumann neighborhood of radius n.
+Number of cells on a surface of an m-dimensional von Neumann neighborhood of radius n.
+ */
+
+
+public class DelannoyNumber {
+    public static void main(String[] args) {
+//        System.out.println(amount(45.88));
+        System.out.println(amount2(1050));
+        System.out.println(delannoyfib(3,3));
+        System.out.println(delannoy(3,4));
+    }
+    public static String amount(double amount)
+    {
+        StringBuilder sb = new StringBuilder();
+        amount=(amount*100);
+        sb.append((int)amount);
+        String str=  "00000000"+sb.toString();
+        return str;
+    }
+    public static String amount2(double amount)
+    {
+        int amountinPaise= (int)(amount*100);
+        String formatted= String.format("%012d",amountinPaise);
+        return formatted;
+    }
+    public static int delannoyfib(int m, int n)
+    {
+        if(m==0 ||n==0)
+            return 1;
+
+        return delannoyfib(m-1,n-1)+delannoyfib(m-1,n)+delannoyfib(m,n-1);
+    }
+    public static int delannoy(int m, int n)
+    {
+        int ans[][]= new int[m+1][n+1];
+        for (int i = 0; i <=m ; i++) {
+            ans[i][0]=1;
+        }
+        for (int i = 0; i <=n ; i++) {
+            ans[0][i]=1;
+        }
+        for (int i = 1; i <=m ; i++) {
+            for (int j = 1; j <=n ; j++) {
+                ans[i][j]=ans[i-1][j-1]+ans[i-1][j]+ans[i][j-1];
+            }
+        }
+
+
+        return ans[m][n];
+    }
+}
